561d14fe65d96c8dabc3b81f164afbf0bafa08c0
Akshay Rajeev Nambiar <akshayrajeevnambiar@gmail.com>
2026-01-26 14:39:28 -0500

    String concatnation and user inputs

---
+++ 1. String concatnation and user inputs/bandNameGenerator.py
@@ -0,0 +1,7 @@
+# Band Name Generator
+# Program to understand string concatenation and user input
+
+city = input("What city did you grow up in ")
+petname = input("What is your pet name ")
+
+print("Your band name is " + city + " " + petname)
